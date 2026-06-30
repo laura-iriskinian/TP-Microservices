70e1f8f71f60eb36be8ae90284c8dc5b37ce8de6
Laura-2115 <laura.iriskinian@laplateforme.io>
2026-06-30 14:02:04 +0200

    init + idendity-service

---
+++ identity-service/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const PORT = 3001;
+
+const users = [
+    {id: 1, name: 'Pierre'}, 
+    {id: 2, name: 'Paul'}, 
+    {id: 3, name: 'Jacques'}
+];
+
+app.get("/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((u) => u.id === id);
+
+    if (!user) {
+        return res.status(404).json({error: "Utilisateur introuvable"});
+    }
+
+    return res.status(200).json(user);
+});
+
+app.get("/users", (req, res) => {
+    return res.status(200).json(users);
+});
+
+app.listen(PORT, () => {
+    console.log(`Identity service en écoute sur le port ${PORT}`);
+});
